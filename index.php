392c0f1db429ba34d09152f681d61f5c8733a9a0
Sistema3 <sistema3@engepecas.com.br>
2025-05-20 09:05:06 -0300

    fix nl1

--- index.php
+++ index.php
@@ -188,7 +188,8 @@
 
     <!-- <nav class="navbar navbar-expand navbar-light" 
     style="position: fixed; top: 0; height: 60px; width: 100%; z-index: 1000; background-color: rgba(0, 0, 0, 0.9);"> -->
-    <nav class="navbar bg-dark border-bottom border-body fixed-top" data-bs-theme="dark">
+    <nav class="navbar bg-dark border-bottom border-body fixed-top" data-bs-theme="dark"
+    style="position: fixed; top: 0; height: 60px; width: 100%; z-index: 1000; background-color: rgba(0, 0, 0, 0.9);">
         <div class="container">
             <div class="navbar-nav mx-auto">
                 <a href="#inicio" class="nav-item nav-link" style="color: #ffffff;"><i class="fa fa-home"></i></a>
